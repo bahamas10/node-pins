7fc03e6160ab2d0d2130475aeaf75268688a53d7
Dave Eddy <dave@daveeddy.com>
2013-11-12 19:42:56 -0500

    fix bad range transfer

--- routes/static.js
+++ routes/static.js
@@ -151,6 +151,11 @@ function main(opts) {
               endrange = stats.size - 1;
             var chunksize = endrange - startrange + 1;
 
+            if (endrange <= startrange) {
+              end(res, 500);
+              return;
+            }
+
             res.statusCode = 206;
             res.setHeader('Content-Range', 'bytes ' + startrange + '-' + endrange + '/' + stats.size);
             res.setHeader('Accept-Ranges', 'bytes');
